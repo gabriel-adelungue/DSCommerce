225f951c9fb9d319f27570740788919f193e85a3
gadelungue <gadelungue@minsait.com>
2025-04-02 20:14:26 -0300

    Adicionando opcção de getOrder e getProduct

--- src/main/java/com/gadelungue/dscommerce/entities/Product.java
+++ src/main/java/com/gadelungue/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.gadelungue.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -83,4 +84,13 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
+    }
+
 }
